4fbcf9acb219ac09b3b6fd37942c85e8e5e2c7a3
Charlspuri27 <142780800+Charlspuri27@users.noreply.github.com>
2026-02-01 16:53:48 +1100

    Update UV config with new properties and fixes
    Added 'bare' and 'wisp' properties, fixed quotes in the configuration.

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,12 +1,13 @@
-// This file overwrites the stock UV config.js
-
 self.__uv$config = {
-	prefix: "/uv/service/",
-	encodeUrl: Ultraviolet.codec.xor.encode,
-	decodeUrl: Ultraviolet.codec.xor.decode,
-	handler: "/uv/uv.handler.js",
-	client: "/uv/uv.client.js",
-	bundle: "/uv/uv.bundle.js",
-	config: "/uv/uv.config.js",
-	sw: "/uv/uv.sw.js",
+    prefix: "/uv/service/",
+    bare: "/bare/", 
+    // FIXED: Removed the extra https:// and fixed the quotes
+    wisp: "wss://silky-rubia-proxy-servers-6bfe5d14.koyeb.app/wisp/", 
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: "/uv/uv.handler.js",
+    client: "/uv/uv.client.js",
+    bundle: "/uv/uv.bundle.js",
+    config: "/uv/uv.config.js",
+    sw: "/uv/uv.sw.js",
 };
